41dc86bb465a034251df3b9f83d58f7b43edc7b8
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-29 10:18:31 +0530

    push AddressBook

---
+++ AddressBook.cs
@@ -0,0 +1,60 @@
+﻿
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookIO
+{
+    public class AddressBook
+    {
+
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public int Zipcode;
+        public string state;
+        public string phonenumber;
+        public string email;
+        //uc1
+        public AddressBook(string firstName, string lastName, string address, int zipcode, string State, string Phonenumber, string Email)
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Zipcode = zipcode;
+
+            state = State;
+            phonenumber = Phonenumber;
+            email = Email;
+        }
+    }
+
+    //uc6
+    public class AdressBook2
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public int Zipcode;
+        public string state;
+        public string phonenumber;
+        public string email;
+
+        public AdressBook2(string firstName, string lastName, string address, int zipcode, string State, string Phonenumber, string Email)
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Zipcode = zipcode;
+
+            state = State;
+            phonenumber = Phonenumber;
+            email = Email;
+        }
+    }
+}
